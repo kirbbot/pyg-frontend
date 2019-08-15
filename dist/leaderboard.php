d7f10d651a05544e395fefaad09d6fb0ceb972e3
Michelle Kirby <michellekirby@Michelles-MacBook-Pro.local>
2019-08-15 09:45:07 -0700

    Leaderboard markup and styles complete

--- dist/leaderboard.php
+++ dist/leaderboard.php
@@ -21,13 +21,38 @@
         </nav>
         <div id="Leaderboard" class="content">
                 <ul class="leaderboard-list">
-                        <li>
-                              <span class="leaderboard-rank"></span>
-                              <img class="leaderboard-avatar" src="#"  />
-                              <span class="leaderboard-name"></span>
-                              <span class="leaderboard-change"></span>
-                              <span class="leaderboard-amount"></span>
+                        <li class="leaderboard-list-item">
+                                <a class="leaderboard-list-item-link" href="gamer_profile.php">
+                                </a>
+                                <span class="leaderboard-list-item-rank">1</span>
+                                <img class="leaderboard-list-item-avatar" src="img/placeholder-user-avatar.png"/>
+                                <span class="leaderboard-list-item-name">Firstname Lastname</span>
+                                <span class="leaderboard-list-item-change icon-block leaderboard-list-item-change-icon_up">navigateup</span>
+                                </span>
+                                <span class="leaderboard-list-item-amount">$42,543</span>
                         </li>
+                        <li class="leaderboard-list-item">
+                                <a class="leaderboard-list-item-link" href="gamer_profile.php">
+                                </a>
+                                <span class="leaderboard-list-item-rank">2</span>
+                                <img class="leaderboard-list-item-avatar" src="img/placeholder-user-avatar.png"/>
+                                <span class="leaderboard-list-item-name">Firstname Lastname</span>
+                                <span class="leaderboard-list-item-change icon-block leaderboard-list-item-change-icon_down">navigatedown</span>
+                                </span>
+                                <span class="leaderboard-list-item-amount">$41,698</span>
+                        </li>
+                        <?php for($i=2; $i<=40; $i++){ ?>
+                        <li class="leaderboard-list-item">
+                                <a class="leaderboard-list-item-link" href="gamer_profile.php">
+                                </a>
+                                <span class="leaderboard-list-item-rank"><?php echo $i ?></span>
+                                <img class="leaderboard-list-item-avatar" src="img/placeholder-user-avatar.png"/>
+                                <span class="leaderboard-list-item-name">Firstname Lastname</span>
+                                <span class="leaderboard-list-item-change icon-block leaderboard-list-item-change-icon_down"></span>
+                                </span>
+                                <span class="leaderboard-list-item-amount">$20,000</span>
+                        </li>
+                        <?php } ?>
                 </ul> <!--end leaderboard-list -->
         </div> <!--end leaderboard-->
 </body>
